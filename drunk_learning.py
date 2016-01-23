cb3ba7e541f59987485d61b878ce068827d1e018
Jack Beckwith <jackbeckwith@gmail.com>
2016-01-23 16:51:17 -0500

    Update drunk_learning.py

--- drunk_learning.py
+++ drunk_learning.py
@@ -1,96 +1,77 @@
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.externals import joblib
 from sklearn import preprocessing
 
-# create sample data set
-X = np.random.random_sample([500,20])
-y = np.random.randint(0,2,[500])
-
-n,d = X.shape
-nTrain = int(0.8*n)  #training on 50% of the data
-
-# shuffle the data
-idx = np.arange(n)
-np.random.seed(13)
-np.random.shuffle(idx)
-X = X[idx]
-y = y[idx]
-
-# split the data
-Xtrain = X[:nTrain,:]
-ytrain = y[:nTrain]
-Xtest = X[nTrain:,:]
-ytest = y[nTrain:]
-
-# Standardize data
-scaler = preprocessing.StandardScaler().fit(X)
-Xtrain = scaler.transform(Xtrain)
-Xtest = scaler.transform(Xtest)
-
-# train the logistic regression
-# logistic_model = LogisticRegression()
-# logistic_model.fit(Xtrain, ytrain)
-# y_preds_logistic = logistic_model.predict(Xtest)
-# accuracy = accuracy_score(ytest, y_preds_logistic)
-# print accuracy
-
-# train naive bayes
-clf = GaussianNB()
-clf.fit(Xtrain, ytrain)
-y_pred = clf.predict(Xtest)
-accuracy = accuracy_score(ytest, y_pred)
-print accuracy
-
-filename = 'test_sklearn.pkl'
-_ = joblib.dump(clf, filename, compress=9)
-
-clf = joblib.load(filename)
-print clf
-
-# clf.partial_fit(Xtest, ytest)
-y_pred = clf.predict(Xtrain)
-accuracy = accuracy_score(ytrain, y_pred)
-print accuracy
-
-class DrunkLearning(object):
+class DrunkLearningBatch(object):
     """drunk_learning class"""
     def __init__(self):
-        self.clf = LogisticRegression()
-        self.filename = 'modelLogReg.pkl'
-
-    def predict(self, X):
-        ret = self.clf.predict(X)
-        return str(ret[0])
+        self.clf = SVC()
+        self.filename = 'modelSVM.pkl'
 
     def fit(self, X, y):
+    	X = np.array([X])
+        y = np.array(y)
         self.clf.fit(X, y)
         joblib.dump(self.clf, self.filename, compress=9)
 
-class DrunkLearningSVM(DrunkLearning):
-    """drunk_learning class"""
-    def __init__(self):
-        super(DrunkLearningSVM, self).__init__()
-        self.clf = LogisticRegression()
-        self.filename = 'modelSVM.pkl'
+    def predict(self, X):
+    	X = np.array([X])
+        ret = self.clf.predict(X)
+        return str(ret[0])
 
-    def gridSearch(self, X, y):
-        tuned_parameters = [{'kernel': ['poly'], 'degree': [2, 3],
-                            'C': [1500, 2000, 3000]}]
-        self.clf = GridSearchCV(SVM(C=1, kernel='poly'), tuned_parameters, cv=10)
-
-class DrunkLearningNB(DrunkLearning):
-    """drunk_learning class"""
+class DrunkLearningAdaBoost(DrunkLearningBatch):
+    """drunk_learning implementation of AdaBoost"""
     def __init__(self):
-        super(DrunkLearningNB, self).__init__()
-        self.clf = GaussianNB()
-        self.filename = 'modelNB.pkl'
+        super(DrunkLearningAdaBoost, self).__init__()
+        self.clf = AdaBoostClassifier(base_estimator=DecisionTreeRegressor(max_depth=None), n_estimators=100)
+        self.filename = 'modelAdaBoost.pkl'
+
+class DrunkLearningRandomForest(DrunkLearningBatch):
+    """drunk_learning implementation of a Random Forest Classifier"""
+    def __init__(self):
+        super(DrunkLearningRandomForest, self).__init__()
+        self.clf = RandomForestClassifier(n_estimators = 250, max_features='sqrt', max_depth = None)
+        self.filename = 'modelRandomForest.pkl'
+
+class DrunkLearningOnline(DrunkLearningBatch):
+    """drunk_learning class for online learning"""
+    def __init__(self):
+    	super(DrunkLearningOnline, self).__init__()
+        self.clf = Perceptron()
+        self.filename = 'modelPerceptron.pkl'
 
     def partial_fit(self, X, y):
         X = np.array([X])
         y = np.array(y)
         self.clf.partial_fit(X, y, [0, 1])
         joblib.dump(self.clf, self.filename, compress=9)
+
+class DrunkLearningOnlineSVM(DrunkLearningOnline):
+    """drunk_learning implementation of Online Linear SVM"""
+    def __init__(self):
+        super(DrunkLearningOnlineSVM, self).__init__()
+        self.clf = SGDClassifier(loss="hinge", penalty="l2")
+        self.filename = 'modelOnlineSVM.pkl'
+
+class DrunkLearningNB(DrunkLearningOnline):
+    """drunk_learning implementation of Online Naive Bayes"""
+    def __init__(self):
+        super(DrunkLearningNB, self).__init__()
+        self.clf = GaussianNB()
+        self.filename = 'modelNB.pkl'
+
+class DrunkLearningPassiveAggressive(DrunkLearningOnline):
+    """drunk_learning implementation of Online Passive Aggressive Classifier"""
+    def __init__(self):
+        super(DrunkLearningPassiveAggressive, self).__init__()
+        self.clf = PassiveAggressiveClassifier(loss='hinge', C=1.0))
+        self.filename = 'modelPassiveAggressive.pkl'
 
